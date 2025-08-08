00bdebdd548a052c0c8c33629f5bda38694d8059
GoRhanHee <gorhanhee01@gmail.com>
2025-08-08 17:44:51 +0900

    Add G986N G988N
    Add G986N G988N Defconfig, Device Tree..etc

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+MODEL=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+
+case "$MODEL" in
+    G981N )
+        DEVICE="x1q"
+        ;;
+    G986N )
+        DEVICE="y2q"
+        ;;
+    G988N )
+        DEVICE="z3q"
+        ;;               
+    * )
+        echo "Check Your Model! EX)./build.sh G981N"
+        exit 1
+        ;;
+esac
+
 export ARCH=arm64
 
 CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
@@ -12,28 +30,59 @@ LOCATION=$(pwd)
 OUT_DIR=$(pwd)/out
 
 mkdir out
-mkdir gorhanhee
+
+GORHANHEE="$(pwd)/gorhanhee"
+
+if [ -d "$GORHANHEE" ]; then
+    rm -rf "$GORHANHEE"/*
+else
+    mkdir -p "$GORHANHEE"
+fi
 
 make -j16 -C $(pwd) O=$OUT_DIR mrproper
-make -j16 -C $(pwd) O=$OUT_DIR $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE REAL_CC=$CLANG CLANG_TRIPLE=$CLANG_TRIPLE vendor/x1q_kor_singlex_defconfig
+make -j16 -C $(pwd) O=$OUT_DIR $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE REAL_CC=$CLANG CLANG_TRIPLE=$CLANG_TRIPLE vendor/${DEVICE}_kor_singlex_defconfig
 make -j16 -C $(pwd) O=$OUT_DIR $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE REAL_CC=$CLANG CLANG_TRIPLE=$CLANG_TRIPLE || exit 1
 
-cp "$IMAGE_DTB" "$(pwd)/AIK/split_img/boot.img-kernel"
-cd $(pwd)/AIK
+cp "$IMAGE_DTB" "$(pwd)/AIK_${DEVICE}/split_img/boot.img-kernel"
+cd $(pwd)/AIK_${DEVICE}
 ./repackimg.sh
 
 cd "$LOCATION"
-cp "$(pwd)/AIK/image-new.img" "$(pwd)/gorhanhee/boot.img"
+cp "$(pwd)/AIK_${DEVICE}/image-new.img" "${GORHANHEE}/boot.img"
 
-python3 mkdtboimg.py create dtbo.img \
+case "${MODEL}" in
+    G981N )
+		python3 mkdtboimg.py create dtbo.img \
   	--page_size=4096 \
   	--version=0 \
   	--id=0x0 --rev=0x0 --custom0=0x0 --custom1=0x0 --custom2=0x0 --custom3=0x0 \
   	${OUT_DIR}/arch/arm64/boot/dts/samsung/common/kona-sec-system-update-overlay.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
   	${OUT_DIR}/arch/arm64/boot/dts/samsung/x1q/kona-sec-x1q-kor-overlay-r13.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
   	${OUT_DIR}/arch/arm64/boot/dts/samsung/x1q/kona-sec-x1q-kor-overlay-r14.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0
+		;;
+    G986N )
+		python3 mkdtboimg.py create dtbo.img \
+  	--page_size=4096 \
+  	--version=0 \
+  	--id=0x0 --rev=0x0 --custom0=0x0 --custom1=0x0 --custom2=0x0 --custom3=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/y2q/kona-sec-y2q-kor-overlay-r13.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/y2q/kona-sec-y2q-kor-overlay-r14.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/common/kona-sec-system-update-overlay.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0	
+		;;
+    G988N )
+		python3 mkdtboimg.py create dtbo.img \
+  	--page_size=4096 \
+  	--version=0 \
+  	--id=0x0 --rev=0x0 --custom0=0x0 --custom1=0x0 --custom2=0x0 --custom3=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/common/kona-sec-system-update-overlay.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/z3q/kona-sec-z3q-kor-overlay-r14.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/z3q/kona-sec-z3q-kor-overlay-r13.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/z3q/kona-sec-z3q-kor-overlay-r16.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0 \
+  	${OUT_DIR}/arch/arm64/boot/dts/samsung/z3q/kona-sec-z3q-kor-overlay-r15.dtbo --custom0=0x00 --custom1=0x00 --id=0x0 --rev=0x0
+		;;
+esac
 
-mv "$(pwd)/dtbo.img" "$(pwd)/gorhanhee/dtbo.img"
+mv "$(pwd)/dtbo.img" "${GORHANHEE}/dtbo.img"
 
-cd gorhanhee
-tar -cvf x1q_SuSFS.tar boot.img dtbo.img
+cd ${GORHANHEE}
+tar -cvf ${DEVICE}_SuSFS.tar boot.img dtbo.img
