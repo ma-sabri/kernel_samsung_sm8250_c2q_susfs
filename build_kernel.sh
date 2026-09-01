47deda5db8b3161a608728e87a39dbd05a37e37c
Munirg2003 <munirg2003@gmail.com>
2026-09-01 14:49:15 +0500

    fix: isolate compilation to standalone shell script to fix yaml parser crash

---
+++ build_kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "=== Setting Up KernelSU-Next ==="
+rm -rf KernelSU-Next
+git clone --depth=1 https://github.com KernelSU-Next
+
+echo "=== Configuring Environment Variables ==="
+export ARCH=arm64
+export SUBARCH=arm64
+
+echo "=== Patching Defconfig Loop ==="
+cp arch/arm64/configs/vendor/samsung/defconfig arch/arm64/configs/samsung_ci_defconfig
+
+echo "=== Generating Configs ==="
+mkdir -p out
+make O=out ARCH=arm64 samsung_ci_defconfig
+
+echo "=== Beginning Compilation Pipeline ==="
+make O=out ARCH=arm64 -j$(nproc --all)
